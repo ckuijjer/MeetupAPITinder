32ec809ba55d267281e9354d489157ad21e60972
Casper Kuijjer <casper@kuijjer.com>
2017-02-19 17:17:06 +0100

    some ui thingies

---
+++ js/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+import _ from 'lodash';
+
+import Tinder from './Tinder';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      members: [],
+    };
+
+    fetch('https://api.meetup.com/WebDevelopment-Nederland/events/237701686/rsvps?photo-host=public&sig_id=148936292&sig=791565ae64fe83088cdcd2bc5d464b7a715c7f47')
+      .then(response => response.json())
+      .then((json) => {
+        const members = json.map(item => {
+          return {
+            name: _.get(item, 'member.name', 'no name'),
+            imageUrl: _.get(item, 'member.photo.highres_link', 'http://combonetwork.com/img/empty_profile.png'),
+          };
+        });
+
+        this.setState({ members });
+      });
+  }
+
+  render() {  
+    if (this.state.members.length) {
+      return <Tinder members={this.state.members} />
+    } else {
+      return <View />;
+    }
+  }
+}
+
+export default App;
--- js/Tinder.js
+++ js/Tinder.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions
+} from 'react-native';
+
 
 import SwipeCards from 'react-native-swipe-cards';
 
-let Card = React.createClass({
-  render() {
-    return (
-      <View style={[styles.card, {backgroundColor: '#f99'}]}>
-        <Image
-          style={{width: 200, height: 200, backgroundColor: '#99f'}}
-          source={{ uri: this.props.imageUrl }}
-        />
-        <Text>{this.props.name}</Text>
-      </View>
-    )
+const styles = StyleSheet.create({
+  card: {
+    height: 400,
+    padding: 20,
+    backgroundColor: '#fbfbfb',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#ccc',
+    borderRadius: 10,
+    shadowColor: '#666', // shadow doesn't work on Android, perhaps use https://github.com/879479119/react-native-shadow
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.3
+  },
+  image: {
+    flex: 1,
+    height: 600,
+  },
+  text: {
+    marginTop: 16,
+    fontSize: 24,
+    color: '#333',
+    textAlign: 'center',
   }
-})
 
+});
+
+const Card = ({ name, imageUrl }) => {
+  const { width } = Dimensions.get('window');
+  const cardWidth = width - 40;
+
+  return (
+    <View style={[styles.card, { width: cardWidth }]}>
+      <Image
+        style={styles.image}
+        source={{ uri: imageUrl }}
+        resizeMode="cover"
+      />
+      <Text style={styles.text}>{name}</Text>
+    </View>
+  );
+}
 
 export default React.createClass({
   handleYup (card) {
@@ -44,13 +82,3 @@ export default React.createClass({
 const NoMoreCards = () => (
   <View style={[styles.card, {backgroundColor: '#f99'}]} />
 );
-
-const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 300,
-    height: 300,
-  }
-})
